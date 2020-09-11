5663b29817bbceea28604ea1a6f02d830e935385
azj5357 <azj5357@psu.edu>
2020-09-11 19:53:26 +0000

    run function

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ def getGradePoint(letter):
         return letter_gradepoint[letter]
     return 0.0
 
-total_creds = 0.0
-total_gradepoint = 0.0
-for i in range(1, 4):
-    letter_grade = input("Enter your course {} letter grade: ".format(i))
-    creds = float(input("Enter your course {} credit: ".format(i)))
-    gp = getGradePoint(letter_grade)
-    total_gradepoint = total_gradepoint + gp * creds
-    total_creds = total_creds + creds
-    print("Grade point for course {} is: {}".format(i, gp))
+def run():
 
-print("Your GPA is: {}".format(total_gradepoint / total_creds))
+    total_creds = 0.0
+    total_gradepoint = 0.0
+    for i in range(1, 4):
+      letter_grade = input("Enter your course {} letter grade: ".format(i))
+      creds = float(input("Enter your course {} credit: ".format(i)))
+      gp = getGradePoint(letter_grade)
+      total_gradepoint = total_gradepoint + gp * creds
+      total_creds = total_creds + creds
+      print("Grade point for course {} is: {}".format(i, gp))
+
+      print("Your GPA is: {}".format(total_gradepoint / total_creds))
 
 
-
+if __name__ == "__main__":
+  run()
 
